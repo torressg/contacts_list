40f047440fac03c2080db044972838df35bc1631
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2023-10-13 19:43:10 -0300

    create UI add contact done

--- lib/pages/add_contact.dart
+++ lib/pages/add_contact.dart
@@ -44,7 +44,8 @@ class _AddContactState extends State<AddContact> {
                               leading: Icon(Icons.photo_library),
                               title: Text("Galeria"),
                               onTap: () async {
-                                final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+                                final XFile? image = await _picker.pickImage(
+                                    source: ImageSource.gallery);
                               },
                             )
                           ],
@@ -60,10 +61,29 @@ class _AddContactState extends State<AddContact> {
               child: TextFormField(
                 decoration: const InputDecoration(
                     labelText: "Nome do contato",
-                    hintText: "Digite o nome do contato",
+                    hintText: "Digite o nome do contato.",
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(0)))),
               ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: TextFormField(
+                keyboardType: TextInputType.phone,
+                decoration: const InputDecoration(
+                    labelText: "Número do contato",
+                    hintText: "Digite o número do contato.",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(0)))),
+              ),
+            ),
+            Container(
+              child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "SALVAR",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  )),
             )
           ],
         ),
